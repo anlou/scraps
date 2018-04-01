f262a2c414a88a3cc6c52220f2a7622a3abf6191
LA_Home <an.lou@mail.ru>
2018-04-01 19:22:24 +0300

    01/04

--- base/vue.php
+++ base/vue.php
@@ -1,23 +1,12 @@
 <ul class="collapsible popout" data-collapsible="accordion">
-
+    <div class="link">
+        <a href="https://ru.vuejs.org/v2/guide/index.html">Vue Doc</a>
+    </div>
 	<li>
 		<div class="collapsible-header">Рендеринг</div>
 		<div class="collapsible-body">
-			<pre data-enlighter-language="html">
-<div id="app">
-  {{ message }}
-</div>
-			</pre>
-			<pre data-enlighter-language="javascript">
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Привет, Vue!'
-  }
-})
-			</pre>
 			<h5>Общие принципы</h5>
-			<p>Создание улемента vue:</p>
+			<p>Создание элемента vue:</p>
 			<pre data-enlighter-language="javascript">
 var ИМЯ_ОБЬЕКТА = new Vue({
   el: '#СЕЛЕКТОР ПО ID(ИЛИ ПО CLASS - ".")',
@@ -26,28 +15,17 @@ var ИМЯ_ОБЬЕКТА = new Vue({
   }
 })
 			</pre>
-            <pre data-enlighter-language="html">
-<div id="app-2">
-  <span v-bind:title="message">
-    Наведи курсор на меня пару секунд,
-    чтобы увидеть динамически связанное значение title!
-  </span>
-</div>
-            </pre>
-            <pre data-enlighter-language="javascript">
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
-  }
-})
-            </pre>
             <div class="card-panel">
-    Здесь мы встречаемся с чем-то новым. Атрибут <b>v-bind</b>, который вы видите, называется директивой. Директивы имеют префикс <b>v-</b>, указывающий на их особую природу. Как вы уже могли догадаться, они применяют к отображаемому DOM особое реактивное поведение, управляемое Vue. В данном примере директива говорит “сохраняй значение title этого элемента актуальным при изменении свойства message у экземпляра Vue”.
-
-    Если вы снова откроете консоль JavaScript и введёте app2.message = 'какое-то новое сообщение', вы опять-таки увидите, что связанный код HTML — в данном случае, атрибут title — обновился.
+                Директивы имеют префикс <b>v-</b>, указывающий на их особую природу. Как вы уже могли догадаться, они применяют к отображаемому DOM особое реактивное поведение, управляемое Vue. Например:
+                <ul>
+                    <li><b>v-bind</b></li>
+                    <li><b>v-if</b></li>
+                    <li><b>v-for</b></li>
+                    <li><b>v-on:</b></li>
+                    <li><b>v-model</b></li>
+                </ul>
             </div>
-		</div>
+        </div>
 	</li>
 
 
